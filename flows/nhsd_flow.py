ac6c75973a0f514b1985fcaefb78bbfcf959c5d9
mp39 <mp39@sanger.ac.uk>
2023-03-06 19:47:27 +0000

    NHSD flow changes

--- flows/nhsd_flow.py
+++ flows/nhsd_flow.py
@@ -1,6 +1,5 @@
 from prefect_shell import shell_run_command
 from prefect import flow, task
-from prefect_email import email_send_message, EmailServerCredentials
 import os
 import pymysql
 from datetime import datetime
