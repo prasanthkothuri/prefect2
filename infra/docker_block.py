1dee25318ace4234e0901d070104579d22c657e3
Prasanth Kothuri <vk9@sanger.ac.uk>
2023-08-23 16:24:57 +0100

    Update infra/docker_block.py

--- infra/docker_block.py
+++ infra/docker_block.py
@@ -12,6 +12,7 @@ history_server_port = os.environ['history_server_port']
 image_tag = os.environ['image_tag']
 image = f"gitlab-registry.internal.sanger.ac.uk/pam-dt4/spark-service/spark-service-docker/spark-app:{image_tag}"
 mdb_mysql_password = os.environ['mdb_mysql_password']
+mlwh_mysql_password = os.environ['mlwh_mysql_password']
 
 # set the variables required for docker block
 dockerenv = {"env": env,
@@ -24,7 +25,8 @@ dockerenv = {"env": env,
              "history_server_port": history_server_port,
              "history_server_ip": history_server_ip,
              "PYTHONPATH": "/opt/covid_reports:/opt/prefect",
-             "mdb_mysql_password": mdb_mysql_password
+             "mdb_mysql_password": mdb_mysql_password,
+             "mlwh_mysql_password": mlwh_mysql_password
              }
 volumes = [
     "/k8s:/k8s",
